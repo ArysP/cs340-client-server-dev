f3391cc9b929b8e9afef743ba95396b7e9ba25d9
Arys <aryspena@gmail.com>
2021-07-24 16:13:51 -0700

    Connect to DB. Load data to database

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Repo to store Python and MongoDB code for Client/Server Development
 ## Setup
 - Create a Virtual Environment by navigating to Settings > Projects > Project Interpreter > Clicking the Cog button in the top right > Click "Add" > New venv box checked > Click "OK" > Click "OK"
 - Install the requirements file by typing `pip install -r requirements.txt` in the terminal.
+
+## Environment Variables
+Create a `.env` file in the root of this repo and fill in the following environment variables.
+
+```dotenv
+USERNAME='user'
+PASSWORD='pass'
+DATABASE='database_name'
+```
--- mongo.py
+++ mongo.py
@@ -11,39 +11,102 @@ A method that queries for documents from a specified MongoDB database and specif
 
 Important: Be sure to use find() instead of find_one() when developing your method.
 """
+from collections import OrderedDict
 
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
-
+import logging
+import csv
 
 # Environment variables are read in from an .env file
-username = os.getenv('USERNAME', "Set the username env variable")
-password = os.getenv('PASSWORD', "Set the password env variable")
-port = os.getenv('PORT')
+MONGO_USER = os.getenv("MONGO_USER", "Set the username env variable")
+MONGO_PASS = os.getenv("MONGO_PASS", "Set the password env variable")
+MONGO_HOST = os.getenv("MONGO_HOST", "Set the host env variable")
+MONGO_DB = os.getenv("MONGO_DB", "Set the db env variable")
 
 
 class AnimalShelter(object):
     """ CRUD operations for Animal collection in MongoDB """
 
     def __init__(self):
-        # Initializing the MongoClient. This helps to
-        # access the MongoDB databases and collections.
-        # "mongodb+srv://cluster0.wdpb3.mongodb.net/aac"
-        self.client = MongoClient(f'mongodb://%{username}:%{password}@localhost:{port}')
-        self.database = self.client['project']
+        self.logger = logging.getLogger(__name__)
+
+        # Initialize the MongoClient to access the MongoDB databases and collections.
+        uri = "mongodb+srv://{}:{}@{}/{}?authSource=admin".format(
+            MONGO_USER, MONGO_PASS, MONGO_HOST, MONGO_DB
+        )
+        self.client = MongoClient(uri)
+        # self.client = MongoClient(
+        #     host="mongodb+srv://cluster0.wdpb3.mongodb.net/",
+        #     username=username,
+        #     password=password,
+        #     # authSource="admin",
+        #     authMechanism="SCRAM-SHA-256",
+        # )
+        self.logger.info("Connected to db")
+        self.database = self.client["aac"]
+        self.logger.info("Connected to aac.animals")
+
+    def prepare_csv_data(self, file_path):
+        # CSV to JSON Conversion
+        csv_file = open(file_path, "r")
+        csv_reader = csv.DictReader(csv_file)
+
+        db = aac.database
+        header = [
+            "1",
+            "age_upon_outcome",
+            "animal_id",
+            "animal_type",
+            "breed",
+            "color",
+            "date_of_birth",
+            "datetime",
+            "monthyear",
+            "name",
+            "outcome_subtype",
+            "outcome_type",
+            "sex_upon_outcome",
+            "location_lat",
+            "location_long",
+            "age_upon_outcome_in_weeks",
+        ]
+
+        for row in csv_reader:
+            data: dict = {}
+            for field in header:
+                data[field] = row[field]
+            self.create(data)
+            # self.database.animals.insert_one(data)
+            # self.logger.info("Inserted 1 row")
+            # print(f"Inserted {data}")
 
     # Complete this create method to implement the C in CRUD.
     def create(self, data):
         if data is not None:
-            self.database.animals.insert(data)  # data should be dictionary
+            self.database.animals.insert_one(data)  # data should be dictionary
         else:
             raise Exception("Nothing to save, because data parameter is empty")
 
     # Create method to implement the R in CRUD.
 
 
-if __name__ == '__main__':
-    shelter = AnimalShelter()
-    print(shelter)
-    # shelter.create()
+if __name__ == "__main__":
+    aac = AnimalShelter()
+    # print(aac.client.get_database(name="animals"))
+    print(f"Database: {aac.database.name}")
+
+    file_path = "data/aac_shelter_outcomes.csv"
+    aac.prepare_csv_data(file_path)
+
+    # with open(file_path, "r") as read_obj:
+    #     # pass the file object to reader() to get the reader object
+    #     csv_reader = csv.DictReader(read_obj)
+    #     # Pass reader object to list() to get a list of lists
+    #     mylist = list(csv_reader)
+    #     # print(list_of_rows)
+    #     x = animals.insert_many(mylist)
+    #
+    #     # print list of the _id values of the inserted documents:
+    #     print(x.inserted_ids)
